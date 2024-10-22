2e88a8c54c9ce6c7db6a7b246bb02c47cd3dba46
peyotell <peyotell@outlook.com>
2024-10-22 17:25:35 +0300

    release v1.2.1

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Template\Greeting;
+use Template\ItsMyClass;
 
 echo Greeting::greet();
-echo ' ' . ItsMyClass::greet();
+echo ' ';
+echo ItsMyClass::greet();
